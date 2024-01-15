c0061f6eb489a6896b7346e481bfd8976026dfe6
djackson5995 <146674106+djackson5995@users.noreply.github.com>
2024-01-14 18:19:02 -0700

    displayed results from google api

--- src/components/ResultsList.jsx
+++ src/components/ResultsList.jsx
@@ -1,6 +1,23 @@
 const ResultsList = ({ results }) => {
   console.log(results);
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h2>Search Results</h2>
+      <ul>
+        {results.map((book) => (
+          <li key={book.id}>
+            <h3>{book.volumeInfo.title}</h3>
+            {book.volumeInfo.authors && (
+              <p>Authors: {book.volumeInfo.authors.join(", ")}</p>
+            )}
+            {book.volumeInfo.publishedDate && (
+              <p>Published Date: {book.volumeInfo.publishedDate}</p>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ResultsList;
